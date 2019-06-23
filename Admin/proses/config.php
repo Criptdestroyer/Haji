56b6db4ac010cd708ead2ff47019f522109629ed
Criptdestroyer <ahmademir1201@gmail.com>
2019-06-23 16:03:39 +0700

    azure connect

--- Admin/proses/config.php
+++ Admin/proses/config.php
@@ -1,7 +1,7 @@
 <?php
 $db=mysqli_init(); 
 mysqli_real_connect($db, "hajiserver.mysql.database.azure.com", "alfath@hajiserver", "EmirG4nteng", "haji", 3306);
-
+echo $db."hehe";
 if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }

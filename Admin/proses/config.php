a7299dee747ae27b675019130c4f039cd755422c
Criptdestroyer <ahmademir1201@gmail.com>
2019-06-23 15:32:45 +0700

    config rev

--- Admin/proses/config.php
+++ Admin/proses/config.php
@@ -3,8 +3,9 @@ $server = "hajiserver.mysql.database.azure.com";
 $user = "alfath@hajiserver";
 $password = "EmirG4nteng";
 $nama_database = "haji";
-
-$db = mysqli_connect($server, $user, $password, $nama_database);
+$db=mysqli_init(); 
+mysqli_ssl_set($db, NULL, NULL, '/var/www/html/BaltimoreCyberTrustRoot.crt.pem', NULL, NULL);
+mysqli_real_connect($db, "hajiserver.mysql.database.azure.com", "alfath@hajiserver", $password, $nama_database, 3306);
 
 if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());

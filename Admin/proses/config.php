5f0ae2890cec27736e4842cb4016a9280b7e7484
Criptdestroyer <ahmademir1201@gmail.com>
2019-06-23 16:12:26 +0700

    add ssl

--- Admin/proses/config.php
+++ Admin/proses/config.php
@@ -1,5 +1,6 @@
 <?php
 $db=mysqli_init(); 
+mysqli_ssl_set($con, NULL, NULL, '/var/www/html/BaltimoreCyberTrustRoot.crt.pem', NULL, NULL); 
 mysqli_real_connect($db, "hajiserver.mysql.database.azure.com", "alfath@hajiserver", "EmirG4nteng", "haji", 3306);
 if (mysqli_connect_errno($db)) {
     die('Failed to connect to MySQL: '.mysqli_connect_error());

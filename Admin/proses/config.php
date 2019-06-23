9b561e21e53dbf79f05d7044b83480abb9b28519
Criptdestroyer <ahmademir1201@gmail.com>
2019-06-23 15:57:49 +0700

    db

--- Admin/proses/config.php
+++ Admin/proses/config.php
@@ -1,10 +1,7 @@
 <?php
-$server = "hajiserver.mysql.database.azure.com";
-$user = "alfath@hajiserver";
 $password = "EmirG4nteng";
 $nama_database = "haji";
 $db=mysqli_init(); 
-mysqli_ssl_set($db, NULL, NULL, '/var/www/html/BaltimoreCyberTrustRoot.crt.pem', NULL, NULL);
 mysqli_real_connect($db, "hajiserver.mysql.database.azure.com", "alfath@hajiserver", $password, $nama_database, 3306);
 
 if( !$db ){

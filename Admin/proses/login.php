83d904b7ab7ecb653fc3ba4d9a96381fd7702bbd
Criptdestroyer <ahmademir1201@gmail.com>
2019-06-23 15:54:39 +0700

    conn

--- Admin/proses/login.php
+++ Admin/proses/login.php
@@ -5,17 +5,13 @@ if(isset($data['login'])){
     $pass = md5($data['password']);
     $user = $data['username'];
     $sql = "select * from user where username = '$user' and password='$pass'";
-    echo $sql;
     $query = mysqli_query($db, $sql);
-    while($row = $query->fetch_object()){
-        print_r($row);
-    }
     if($query->num_rows == 1){
         session_start();
         $_SESSION["username"] = $user;
         echo "Session variables are set.";
-        // header('Location: ../index.php');
+        header('Location: ../index.php');
     }else{
-        // header('Location: ../login.php');
+        header('Location: ../login.php');
     }
 }

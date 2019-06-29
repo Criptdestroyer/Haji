b0b00fd3a7c29646f0d35a4fb764e06c5dada775
Criptdestroyer <ahmademir1201@gmail.com>
2019-06-29 23:51:10 +0700

    add config

--- Admin/proses/config.php
+++ Admin/proses/config.php
@@ -1,10 +1,10 @@
 <?php
-// $server = "fdb26.awardspace.net";
-// $user = "3085597_haji";
-// $password = "adminhaji8";
-// $nama_database = "3085597_haji";
-$db=mysqli_init(); 
-mysqli_real_connect($db, "haji.mysql.database.azure.com", "adminhaji@haji", "Pweb2019","haji", 3306);
+// $server = "localhost";
+// $user = "root";
+// $password = "";
+// $nama_database = "haji";
+
+$db = mysqli_connect("haji.mysql.database.azure.com", "adminhaji@haji", "Pweb2019","haji");
 
 if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());

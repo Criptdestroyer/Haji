e381f459b04a3ac29b858d1821874432b9a18291
Criptdestroyer <ahmademir1201@gmail.com>
2019-06-29 23:54:34 +0700

    add config

--- Home/proses/config.php
+++ Home/proses/config.php
@@ -1,7 +1,7 @@
 <?php
 $db=mysqli_init();
-mysqli_ssl_set($con, NULL, NULL, NULL, NULL, NULL);
- mysqli_real_connect($con, "haji.mysql.database.azure.com", "adminhaji@haji", "Pweb2019", "haji", 3306);
+mysqli_ssl_set($db, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($db, "haji.mysql.database.azure.com", "adminhaji@haji", "Pweb2019", "haji", 3306);
 
 if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
